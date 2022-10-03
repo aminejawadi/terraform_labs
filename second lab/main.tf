2029a43fbabf9ce0f79a4506041514167ef620d3
Amin Jawadi <aminjawadi@Amins-MacBook-Pro.local>
2022-10-04 00:08:02 +0100

    second lab

--- second lab/main.tf
+++ second lab/main.tf
@@ -107,3 +107,28 @@ allocation_id = aws_eip.nat_gateway_eip.id
     Name = "demo_nat_gateway"
   }
   }
+
+
+
+  # Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
+data "aws_ami" "ubuntu" { 
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+}
+filter {
+  name   = "virtualization-type"
+  values = ["hvm"]
+}
+owners = ["099720109477"]
+}
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server" {
+ami           = data.aws_ami.ubuntu.id
+instance_type = "t2.micro"
+subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+tags = {
+  Name = "Ubuntu EC2 Server"
+}
+}
